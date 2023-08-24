bd2de130b68edd6c6fec7327c2a211697b50f59c
rk-techs <ryskdevelop@gmail.com>
2023-08-24 23:16:51 +0900

    style: rename and formatted

--- src/resources/views/company/create.blade.php
+++ src/resources/views/company/create.blade.php
@@ -15,37 +15,99 @@
             </div>
 
             <form id="inputForm" action="{{ route('company.store') }}" method="POST">
-            @csrf
-            <div class="input-form-block">
-                <div class="input-form-body">
+                @csrf
+                <div class="input-form-block">
+                    <div class="input-form-body">
 
-                    @include('company.components.input', ['id' => 'nameInput', 'label' => '会社名', 'name' => 'name', 'required' => true])
-                    @include('company.components.input', ['id' => 'postalCodeInput', 'label' => '郵便番号', 'name' => 'postal_code'])
-                    @include('company.components.input', ['id' => 'addressInput', 'label' => '住所', 'name' => 'address'])
-                    @include('company.components.input', ['id' => 'telInput', 'label' => '電話番号', 'name' => 'tel'])
-                    @include('company.components.input', ['id' => 'faxInput', 'label' => 'FAX', 'name' => 'fax'])
-                    @include('company.components.input', ['id' => 'emailInput', 'label' => 'Email', 'name' => 'email', 'type' => 'email'])
-                    @include('company.components.input', ['id' => 'websiteInput', 'label' => '会社URL', 'name' => 'website'])
-                    @include('company.components.input', ['id' => 'industryInput', 'label' => '業種', 'name' => 'industry'])
-                    @include('company.components.input', ['id' => 'capitalInput', 'label' => '資本金', 'name' => 'capital'])
-                    @include('company.components.input', ['id' => 'numberOfEmployeesInput', 'label' => '従業員数', 'name' => 'number_of_employees'])
-                    @include('company.components.input', ['id' => 'annualSalesInput', 'label' => '年商', 'name' => 'annual_sales'])
-                    @include('company.components.input', ['id' => 'listedInput', 'label' => '上場しているかどうか', 'name' => 'listed'])
-                    @include('company.components.input', ['id' => 'establishedAtInput', 'label' => '設立日', 'name' => 'established_at'])
-                    @include('company.components.input', ['id' => 'corporateNumberInput', 'label' => '法人番号', 'name' => 'corporate_number'])
-                    @include('company.components.input', ['id' => 'prospectRatingInput', 'label' => '見込み度', 'name' => 'prospect_rating'])
-                    @include('company.components.form-select', [
-                            'label'   => 'テレアポ担当者ID',
-                            'name'    => 'employee_id',
-                            'id'      => 'employeeIdInput',
+                        @include('company.components.form-input', [
+                            'id' => 'nameInput',
+                            'label' => '会社名',
+                            'name' => 'name',
+                            'required' => true,
+                        ])
+                        @include('company.components.form-input', [
+                            'id' => 'postalCodeInput',
+                            'label' => '郵便番号',
+                            'name' => 'postal_code',
+                        ])
+                        @include('company.components.form-input', [
+                            'id' => 'addressInput',
+                            'label' => '住所',
+                            'name' => 'address',
+                        ])
+                        @include('company.components.form-input', [
+                            'id' => 'telInput',
+                            'label' => '電話番号',
+                            'name' => 'tel',
+                        ])
+                        @include('company.components.form-input', [
+                            'id' => 'faxInput',
+                            'label' => 'FAX',
+                            'name' => 'fax',
+                        ])
+                        @include('company.components.form-input', [
+                            'id' => 'emailInput',
+                            'label' => 'Email',
+                            'name' => 'email',
+                            'type' => 'email',
+                        ])
+                        @include('company.components.form-input', [
+                            'id' => 'websiteInput',
+                            'label' => '会社URL',
+                            'name' => 'website',
+                        ])
+                        @include('company.components.form-input', [
+                            'id' => 'industryInput',
+                            'label' => '業種',
+                            'name' => 'industry',
+                        ])
+                        @include('company.components.form-input', [
+                            'id' => 'capitalInput',
+                            'label' => '資本金',
+                            'name' => 'capital',
+                        ])
+                        @include('company.components.form-input', [
+                            'id' => 'numberOfEmployeesInput',
+                            'label' => '従業員数',
+                            'name' => 'number_of_employees',
+                        ])
+                        @include('company.components.form-input', [
+                            'id' => 'annualSalesInput',
+                            'label' => '年商',
+                            'name' => 'annual_sales',
+                        ])
+                        @include('company.components.form-input', [
+                            'id' => 'listedInput',
+                            'label' => '上場しているかどうか',
+                            'name' => 'listed',
+                        ])
+                        @include('company.components.form-input', [
+                            'id' => 'establishedAtInput',
+                            'label' => '設立日',
+                            'name' => 'established_at',
+                        ])
+                        @include('company.components.form-input', [
+                            'id' => 'corporateNumberInput',
+                            'label' => '法人番号',
+                            'name' => 'corporate_number',
+                        ])
+                        @include('company.components.form-input', [
+                            'id' => 'prospectRatingInput',
+                            'label' => '見込み度',
+                            'name' => 'prospect_rating',
+                        ])
+                        @include('company.components.form-select', [
+                            'label' => 'テレアポ担当者ID',
+                            'name' => 'employee_id',
+                            'id' => 'employeeIdInput',
                             'options' => $employees,
                             'displayAttribute' => 'id',
                             'isRequired' => false,
                         ])
 
 
+                    </div>
                 </div>
-            </div>
             </form>
 
         </div>
