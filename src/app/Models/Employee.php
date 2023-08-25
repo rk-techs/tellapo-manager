04d888f4e20e091810143cadf1293ed458a775bb
rk-techs <ryskdevelop@gmail.com>
2023-08-25 20:58:58 +0900

    feat: add belongsTo relationship between Employee and User models

--- src/app/Models/Employee.php
+++ src/app/Models/Employee.php
@@ -4,7 +4,9 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasMany;
+use Illuminate\Database\Eloquent\Relations\HasOne;
 
 class Employee extends Model
 {
@@ -24,7 +26,10 @@ class Employee extends Model
     |--------------------------------------------------------------------------
     |
     */
-
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 
     public function companies(): HasMany
     {
