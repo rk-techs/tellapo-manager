e6927454986eb15fc357a6ac92e44944fb0d9949
rk-techs <ryskdevelop@gmail.com>
2023-08-26 10:51:25 +0900

    update index view with table includes

--- src/resources/views/call-history/index.blade.php
+++ src/resources/views/call-history/index.blade.php
@@ -1,5 +1,25 @@
+@extends('layouts.app')
 
-WIP: display call histories.
-@foreach ($callHistories as $callHistory)
-    <p>{{ $callHistory }}</p>
-@endforeach
+@section('title')
+    TEL履歴 | {{ config('app.name') }}
+@endsection
+
+@section('content')
+    <main class="layout-main">
+        <div class="main-container">
+
+            <div class="content-header-block">
+                <div class="title title-h2">
+                    TEL履歴
+                </div>
+            </div>
+
+            {{-- @include('call-history.includes.index-search') --}}
+
+            {{-- @include('components.alert', ['action' => session('action')]) --}}
+
+            @include('call-history.includes.index-table')
+
+        </div>
+    </main>
+@endsection
