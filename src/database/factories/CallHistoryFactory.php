f8b2e055d50e39a18870df918e6079a49b1903bd
rk-techs <ryskdevelop@gmail.com>
2023-08-26 09:46:38 +0900

    add Factory and Seeder

---
+++ src/database/factories/CallHistoryFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\CallHistory>
+ */
+class CallHistoryFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'called_at'     => $this->faker->dateTime(),
+            'result'        => $this->faker->numberBetween(1, 5),
+            'receiver_info' => $this->faker->name(),
+            'notes'         => $this->faker->sentence()
+        ];
+    }
+}
