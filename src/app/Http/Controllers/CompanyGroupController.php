7c642857be3e3dd438a43e5b16e00eb140a08392
rk-techs <ryskdevelop@gmail.com>
2023-08-27 15:39:01 +0900

    display companies count per group and unassigned companies on company-groups.index

--- src/app/Http/Controllers/CompanyGroupController.php
+++ src/app/Http/Controllers/CompanyGroupController.php
@@ -2,13 +2,16 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Company;
 use App\Models\CompanyGroup;
 
 class CompanyGroupController extends Controller
 {
     public function index()
     {
-        $companyGroups = CompanyGroup::all();
-        return view('company-groups.index', compact('companyGroups'));
+        $companyGroups = CompanyGroup::withCount('companies')->get();
+        $unassignedCompaniesCount = Company::whereNull('company_group_id')->count();
+
+        return view('company-groups.index', compact('companyGroups', 'unassignedCompaniesCount'));
     }
 }
--- src/resources/views/company-groups/includes/index-table.blade.php
+++ src/resources/views/company-groups/includes/index-table.blade.php
@@ -5,18 +5,31 @@
                 <th class="th-cell col-fixed">操作</th>
                 <th class="th-cell">ID</th>
                 <th class="th-cell">グループ名</th>
+                <th class="th-cell">企業数</th>
                 <th class="th-cell">登録日</th>
                 <th class="th-cell">更新日</th>
             </tr>
         </thead>
         <tbody class="table-body">
+            {{-- 未分類 --}}
+            <tr class="table-row is-hoverable">
+                <td class="td-cell col-fixed">-</td>
+                <td class="td-cell">-</td>
+                <td class="td-cell u-min-w-160">未分類</td>
+                <td class="td-cell">{{ $unassignedCompaniesCount }}件</td>
+                <td class="td-cell u-min-w-160">-</td>
+                <td class="td-cell u-min-w-160">-</td>
+            </tr>
+
+            {{-- companyGroups --}}
             @foreach ($companyGroups as $companyGroup)
                 <tr class="table-row is-hoverable">
                     <td class="td-cell col-fixed">
                         {{-- <a href="{{ route('company-groups.edit', ['id' => $companyGroup->id]) }}" class="btn-sm">編集</a> --}}
                     </td>
                     <td class="td-cell">{{ $companyGroup->id }}</td>
-                    <td class="td-cell">{{ $companyGroup->name }}</td>
+                    <td class="td-cell u-min-w-160">{{ $companyGroup->name }}</td>
+                    <td class="td-cell">{{ $companyGroup->companies_count }}件</td>
                     <td class="td-cell u-min-w-160">{{ $companyGroup->created_at }}</td>
                     <td class="td-cell u-min-w-160">{{ $companyGroup->updated_at }}</td>
                 </tr>
