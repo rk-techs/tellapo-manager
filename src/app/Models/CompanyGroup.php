6955474d606dbb40eb5239a93f9b9a9fdf3226f1
rk-techs <ryskdevelop@gmail.com>
2023-08-27 15:26:31 +0900

    add company-group relationships in Company and CompanyGroup models

--- src/app/Models/CompanyGroup.php
+++ src/app/Models/CompanyGroup.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class CompanyGroup extends Model
 {
@@ -12,4 +13,15 @@ class CompanyGroup extends Model
     protected $fillable = [
         'name',
     ];
+
+    /*
+    |--------------------------------------------------------------------------
+    | Relationships
+    |--------------------------------------------------------------------------
+    |
+    */
+    public function companies():HasMany
+    {
+        return $this->hasMany(Company::class);
+    }
 }
