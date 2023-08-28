89996ea6e6a6df878ee9b270e4da7016a5735b51
rk-techs <ryskdevelop@gmail.com>
2023-08-28 21:41:44 +0900

    feat: define gates in AuthServiceProvider for permission levels

--- src/app/Providers/AuthServiceProvider.php
+++ src/app/Providers/AuthServiceProvider.php
@@ -3,7 +3,10 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
+
+use App\Models\User;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Illuminate\Support\Facades\Gate;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +24,14 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        Gate::define('system-admin', function (User $user) {
+            return $user->permission->level <= 1;
+        });
+        Gate::define('admin', function (User $user) {
+            return $user->permission->level <= 2;
+        });
+        Gate::define('staff', function (User $user) {
+            return $user->permission->level <= 3;
+        });
     }
 }
