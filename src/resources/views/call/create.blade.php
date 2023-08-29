1af7a04812c8f57d5200e9167053154d1285dc7b
rk-techs <ryskdevelop@gmail.com>
2023-08-29 20:26:34 +0900

    style: display calls history on companies call create view

--- src/resources/views/call/create.blade.php
+++ src/resources/views/call/create.blade.php
@@ -104,6 +104,40 @@
                 </div>
             </form>
 
+            {{-- TEL履歴 --}}
+            <div class="content-header-block">
+                <div class="title title-h2">
+                    TEL履歴
+                </div>
+            </div>
+            <div class="table-block is-scrollable">
+                <table class="table">
+                    <thead class="table-header">
+                        <tr class="table-row">
+                            <th class="th-cell">ID</th>
+                            <th class="th-cell">担当者</th>
+                            <th class="th-cell">TEL日時</th>
+                            <th class="th-cell">結果</th>
+                            <th class="th-cell">受付情報</th>
+                            <th class="th-cell">メモ</th>
+                        </tr>
+                    </thead>
+                    <tbody class="table-body">
+                        @foreach($company->calls as $call)
+                            <tr class="table-row is-hoverable">
+                                <td class="td-cell">{{ $call->id }}</td>
+                                <td class="td-cell">{{ $call->employee->user->name }}</td>
+                                <td class="td-cell">{{ $call->formatted_called_at }}</td>
+                                <td class="td-cell">{{ $call->result_label }}</td>
+                                <td class="td-cell">{{ $call->receiver_info }}</td>
+                                <td class="td-cell">{{ $call->notes }}</td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+
+
         </div>
     </main>
 
